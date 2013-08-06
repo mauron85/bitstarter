939f1312a65bb3ac28b078b0c466e4499b4183d3
Marian Hello <marian.hello@gmail.com>
2013-08-06 21:25:00 +0200

    web.js

---
+++ web.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+    fs = require('fs');
+
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, response) {
+  var index = fs.readFileSync('index.html');
+  var buf = new Buffer(index);
+  response.send(buf.toString());
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
